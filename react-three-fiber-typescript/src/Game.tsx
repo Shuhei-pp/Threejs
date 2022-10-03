1cd8db27b2a59ef0ac9703a0139db3855a00cf7e
Shuhei-pp <lotakotayota@gmail.com>
2022-10-03 22:36:49 +0900

    たま置いてみた

--- react-three-fiber-typescript/src/Game.tsx
+++ react-three-fiber-typescript/src/Game.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import { Canvas } from '@react-three/fiber'
+import React,{ useState } from 'react'
+import { Canvas, events } from '@react-three/fiber'
 import { DoubleSide } from 'three';
 import { OrbitControls } from '@react-three/drei'
 
 export const Game: () => JSX.Element = () => {
-  const positionArray = [-30,-10,10,30]
+  const positionArray = [-30, -10, 10, 30]
+  const handleClickStick: (stickNumber:number) => void = (stickNumber) => {
+    console.log(stickNumber)
+  }
   return (
     <Canvas camera={{ fov: 50, position: [150, 150, 150] }} style={{ width: "100vh", height: "100vh" }} shadows>
-      
       <OrbitControls />
       
       <directionalLight
@@ -21,8 +23,14 @@ export const Game: () => JSX.Element = () => {
       {positionArray.map((x, i) => {
         return(
           positionArray.map((y, j) => { 
+            const stickPosition: number = i * 4 + j
             return (
-              <mesh position={[x, 50, y]} key={i * 4 + j} castShadow receiveShadow>
+              <mesh
+                position={[x, 50, y]}
+                key={stickPosition}
+                onClick={(event) => handleClickStick(stickPosition)}
+                castShadow receiveShadow
+              >
                 <boxGeometry args={[1, 100, 1]} />
                 <meshPhongMaterial color={"blue"} />
               </mesh>
@@ -30,6 +38,12 @@ export const Game: () => JSX.Element = () => {
           })
         )
       })}
+
+
+      <mesh position={[-10, 8, 10]} castShadow receiveShadow>
+        <sphereBufferGeometry args={[8, 30, 30]} attach="geometry"/>
+        <meshStandardMaterial color="hotpink" />
+      </mesh>
 
       <mesh position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]} receiveShadow>
         <planeGeometry args={[100, 100]} />
